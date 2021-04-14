8c4d6acb3e132e3ef96fb0341cc8bd832f8f3c73
AgneseBettio <bettio.agnese@gmail.com>
2021-04-14 14:46:48 +0200

    inizializzazione

---
+++ Palidroma/script.js
@@ -0,0 +1,12 @@
+// Palidroma
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+
+
+//prompt utente : inserire una parola
+var parola = prompt("inserisci una parola per verificare se è palindroma");
+
+//una funzione per capire se la parola inserita è palindroma
+//prima verifica xl xd
+
+//seconda verifica xd xl
