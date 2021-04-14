de37045c15a6424e7f74175a0b8819ba1a438e1f
AgneseBettio <bettio.agnese@gmail.com>
2021-04-14 15:21:52 +0200

    funzione per validazione parola e non numero

--- Palidroma/script.js
+++ Palidroma/script.js
@@ -4,9 +4,27 @@
 
 
 //prompt utente : inserire una parola
-var parola = prompt("inserisci una parola per verificare se è palindroma");
+var parolaUtente = prompt("inserisci una parola per verificare se è palindroma");
+console.log(parolaUtente);
+//verificare che la parola sia accettata 
+function validazioneParola(parolaUtente) {
+    var NumeriNonAccettati = parseInt(parolaUtente);
+    //se ritorna NaN non è un numero
+    if(!Number.isNaN(NumeriNonAccettati)){
+        return false
+    }
+    return true
+}
+if(!validazioneParola(parolaUtente)){
+    console.log ("sono accettate solo parole")
+} else { 
+    var parolaAnalizzata = parolaUtente;
+    console.log("la parola scritta " + parolaUtente + " è accetta e verrà analizzata dalla funzione")
+}
 
-//una funzione per capire se la parola inserita è palindroma
-//prima verifica xl xd
 
-//seconda verifica xd xl
+    //una funzione per capire se la parola inserita è palindroma
+// var verificaParolaDaSinistra;
+// var verificaParolaDaDestra;
+// function parolaPalindroma( )
+
