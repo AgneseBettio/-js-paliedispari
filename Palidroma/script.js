b76bfc850c59179d702c78feff79ee061f4fc646
AgneseBettio <bettio.agnese@gmail.com>
2021-04-14 16:16:47 +0200

    prima prova palindromo

--- Palidroma/script.js
+++ Palidroma/script.js
@@ -10,21 +10,39 @@ console.log(parolaUtente);
 function validazioneParola(parolaUtente) {
     var NumeriNonAccettati = parseInt(parolaUtente);
     //se ritorna NaN non è un numero
-    if(!Number.isNaN(NumeriNonAccettati)){
+    if (!Number.isNaN(NumeriNonAccettati)) {
         return false
     }
     return true
 }
-if(!validazioneParola(parolaUtente)){
-    console.log ("sono accettate solo parole")
+if (!validazioneParola(parolaUtente)) {
+    console.log("sono accettate solo parole")
 } else { 
-    var parolaAnalizzata = parolaUtente;
-    console.log("la parola scritta " + parolaUtente + " è accetta e verrà analizzata dalla funzione")
+    var parolaAnalizzata = parolaUtente;   
+    console.log("la parola scritta " + parolaUtente + " è accetta e verrà analizzata dalla funzione");
 }
 
 
-    //una funzione per capire se la parola inserita è palindroma
-// var verificaParolaDaSinistra;
-// var verificaParolaDaDestra;
-// function parolaPalindroma( )
+//una funzione per capire se la parola inserita è palindroma
+
+function parolaPalindroma(parolaAnalizzata) {
+    var matchPalindromo;
+    //lettura parola da sinistra
+    for (var i = 0; i < parolaAnalizzata.lenght; i++) {
+        matchPalindromo = 0;
+        //lettura parola da destra
+        for (var j = parolaAnalizzata.lenght; j >= 0; j--) {
+            if (parolaAnalizzata[i] === parolaAnalizzata[j]) {
+                matchPalindromo++;
+            }
+        }
+        if (matchPalindromo === i && matchPalindromo === j) {
+            parolaAnalizzata = true;
+            console.log( parolaAnalizzata + " è palindroma")
+        } else {
+            parolaAnalizzata = false;
+            console.log( parolaAnalizzata + " non è palindroma")
+        }
+    }
+}
 
